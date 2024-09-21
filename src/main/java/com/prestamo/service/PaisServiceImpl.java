9caf764e47cba745a25f86c347340064a4de5bf0
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2024-09-21 16:36:30 -0500

    ajustes

--- src/main/java/com/prestamo/service/PaisServiceImpl.java
+++ src/main/java/com/prestamo/service/PaisServiceImpl.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.prestamo.entity.Pais;
-import com.prestamo.kafka.service.PaisEventService;
 import com.prestamo.repository.PaisRepository;
 
 @Service
