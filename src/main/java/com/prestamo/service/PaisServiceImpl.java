601920d5cee47b13e5ec64018d7abf66baf70890
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2024-09-22 12:21:33 -0500

    finalizado

--- src/main/java/com/prestamo/service/PaisServiceImpl.java
+++ src/main/java/com/prestamo/service/PaisServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.prestamo.entity.Pais;
+import com.prestamo.kafka.service.PaisEventService;
 import com.prestamo.repository.PaisRepository;
 
 @Service
@@ -14,6 +15,8 @@ public class PaisServiceImpl implements PaisService {
 	@Autowired
 	private PaisRepository paisRepository;
 	
+	@Autowired
+	private PaisEventService paisEventService;
 	
 	@Override
 	public List<Pais> findAll() {
@@ -22,7 +25,8 @@ public class PaisServiceImpl implements PaisService {
 
 	@Override
 	public Pais insertaPais(Pais pais) {
-			return paisRepository.save(pais);//Registra en la base de datos
+		paisEventService.publish(pais);//Publica el evento en kafka
+		return paisRepository.save(pais);//Registra en la base de datos
 	}
 
 }
